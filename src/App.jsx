03808f5cbcfe01dacd605e4b715126747fc24786
Anush79 <barbieworld1056.a@gmail.com>
2023-07-17 17:42:01 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  <h1>Hello 
+    { import.meta.env.VITE_SECRET_KEY }   
+    World</h1>
+<p className='text-red-500'>this is anushka jaiswal</p>
+    </>
+  )
+}
+
+export default App
